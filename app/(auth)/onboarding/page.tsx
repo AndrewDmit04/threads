8b2f9ba1ba84ac4286462d945b120dc55aed65aa
AndrewDmit04 <AndrewDmit2021@gmail.com>
2024-11-22 21:19:36 +0000

    intial commit

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main>
+        <UserButton></UserButton>
+    </main>
+  )
+}
+
+export default Page
