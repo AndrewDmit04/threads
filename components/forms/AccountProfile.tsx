920452d826bcd279dc1743ecf6249e9b3a43fe7d
AndrewDmit04 <AndrewDmit2021@gmail.com>
2024-11-25 23:23:22 +0000

    working mongodb and uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,7 +19,8 @@ import Image from 'next/image'
 import * as z from 'zod'
 import { isBase64Image } from "@/lib/utils"
 import { useUploadThing } from "@/lib/uploadthing"
-
+import { updateUser } from "@/lib/actions/user.actions"
+import {usePathname, useRouter} from 'next/navigation'
 interface Props {
     user: {
         id: string,
@@ -35,6 +36,9 @@ interface Props {
 const AccountProfile = ({user, btnTitle} : Props) => {
   const [files,setFiles] = useState<File[]>([])
   const {startUpload} = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname(); 
+
   const form = useForm({
     resolver : zodResolver(UserValidation),
     defaultValues : {
@@ -70,9 +74,25 @@ const AccountProfile = ({user, btnTitle} : Props) => {
     const hasImageChanged = isBase64Image(blob);
     if(hasImageChanged){
       const imgRes = await startUpload(files)
-      if (imgRes && imgRes[0].fileUrl){
-        values.profile_photo = imgRes[0].fileUrl;
+      if (imgRes && imgRes.fileUrl){
+        values.profile_photo = imgRes.fileUrl;
       }
+    }
+
+    await updateUser({
+      username : values.username, 
+      name : values.name, 
+      bio : values.bio,
+      image : values.profile_photo,
+      userId : user.id,
+      path : pathname
+    })
+    
+    if(pathname === '/profile/edit'){
+      router.back()
+    }
+    else{
+      router.push('/')
     }
   }
   return (
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,37 +1,52 @@
-"use server"
+"use server";
 
+import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
-import User from "../models/user.model";
-import { connectToDB } from "../mongoose/mongoose"
 
-export async function updateUser(
-    userId : string,
-    username: string, 
-    name: string, 
-    bio: string, 
-    image: string, 
-    path : string) : Promise<void> {
+
+import User from "../models/user.model";
+
+import { connectToDB } from "../mongoose";
+
+
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
+
+export async function updateUser({
+  userId,
+  bio,
+  name,
+  path,
+  username,
+  image,
+}: Params): Promise<void> {
+  try {
     connectToDB();
 
-    try{
-        await User.findOneAndUpdate(
-            {id : userId},
-            {
-                username : username.toLowerCase(),
-                name,
-                bio,
-                image,
-                onboarded : true
-            },
-            {upsert : true}
-        ); 
-    
-        if(path === '/profile/edit'){
-            revalidatePath(path)
-        }
-    }
-    catch(error){
-        throw new Error(`Failed to create/update user: ${error}`);
-    }
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
 
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
 }
+
