4dcb1cac454265603280db4a698e377186d66952
AndrewDmit04 <AndrewDmit2021@gmail.com>
2024-11-26 02:54:53 +0000

    fetching threads from main

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
 
-const Home = () => {
+import { fetchPosts } from '@/lib/actions/thread.actions';
+import React from 'react'
+import {currentUser} from "@clerk/nextjs/server"
+import ThreadCard from '@/components/cards/ThreadCard';
+const Home = async () => {
+  const result = await fetchPosts(1,30);
+  
+  const user = await currentUser();
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+        <p className="no-result">No Threads found</p>) 
+        : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createAt={post.createAt}
+                comments={post.comments}
+              />
+            ))}
+          </>
+        )}
+
+      </section>
     </>
   )
 }
